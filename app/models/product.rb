1f2972dbad2e6e442a09d3a9df4f50095ac3fb79
NATHAN <nathan.ho28@gmail.com>
2014-11-09 14:51:13 -0500

    Added validations and created method to reformat price within the product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,10 @@
 class Product < ActiveRecord::Base
+	validates :description, :name, presence: true
+  	validates :price_in_cents, numericality: {only_integer: true}
+
+  	def formatted_price
+		price_in_dollars = price_in_cents.to_f/100	#change to a float
+		sprintf("%.2f", price_in_dollars) 	
+		# %.2f - precision for `f' is number of digits after the decimal point 
+	end
 end
